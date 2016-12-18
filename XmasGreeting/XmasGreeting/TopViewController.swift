ad66fed4fc682c2965781efbf976f0703a596a77
Yuki Nishimura <nishimura.fun@gmail.com>
2016-12-19 02:26:27 +0900

    Completed creating App base (#1)
    * Create project
    
    プロジェクト作成．
    
    * Create pages and these visuals
    
    各ページのビジュアルを作成．
    
    * Update README.md

---
+++ XmasGreeting/XmasGreeting/TopViewController.swift
@@ -0,0 +1,25 @@
+//
+//  TopViewController.swift
+//  XmasGreeting
+//
+//  Created by nishimura on 2016/12/18.
+//  Copyright © 2016年 nishimura. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
