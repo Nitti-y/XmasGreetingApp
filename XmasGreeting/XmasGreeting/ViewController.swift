b565ad5c13c31e8da4c72442d6f05ec2d641a9b1
Yuki Nishimura <nishimura.fun@gmail.com>
2016-12-18 23:44:38 +0900

    Create project
    プロジェクト作成．

---
+++ XmasGreeting/XmasGreeting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XmasGreeting
+//
+//  Created by nishimura on 2016/12/18.
+//  Copyright © 2016年 nishimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
