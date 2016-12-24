9f5b68fb8782e23d41e8097826e3482259636ad6
Yuki Nishimura <nishimura.fun@gmail.com>
2016-12-24 13:04:02 +0900

    Add playing BGM functions
    BGM再生の機能を追加．

--- XmasGreeting/XmasGreeting/TopViewController.swift
+++ XmasGreeting/XmasGreeting/TopViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
+import AVFoundation
 
 class TopViewController: UIViewController {
 
+    var audioPlayer: AVAudioPlayer!
+    var audioPath: NSURL!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        prepareBGM()
+        
+        playBGM()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func tapTreeButton(sender: AnyObject) {
+        stopBGM()
+    }
+    
+    func prepareBGM() {
+        audioPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("SnowFlakes", ofType: "mp3")!)
+    }
+    
+    func playBGM() {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOfURL: audioPath)
+            audioPlayer.numberOfLoops = -1
+            audioPlayer.play()
+        } catch {
+            print("AVAudioPlayer error")
+        }
+    }
+    
+    func stopBGM() {
+        audioPlayer.stop()
+    }
 
 }
 
--- XmasGreeting/XmasGreeting/TreeViewController.swift
+++ XmasGreeting/XmasGreeting/TreeViewController.swift
@@ -7,22 +7,42 @@
 //
 
 import UIKit
+import AVFoundation
 
 class TreeViewController: UIViewController {
-
+    
+    var audioPlayer: AVAudioPlayer!
+    var audioPath: NSURL!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        prepareBGM()
+        
+        playBGM()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func retrunTree (segue: UIStoryboardSegue) {
-    
+        
     }
-
+    
+    func prepareBGM() {
+        audioPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("JingleBells", ofType: "mp3")!)
+    }
+    
+    func playBGM() {
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOfURL: audioPath)
+            audioPlayer.numberOfLoops = -1
+            audioPlayer.play()
+        } catch {
+            print("AVAudioPlayer error")
+        }
+    }
+    
 }
